4cbe3e7213d0ee8dcf48b949c10ec1e8f9379697
OscarMauricio666 <mauriciorondonfajardo@gmail.com>
2021-01-09 11:54:44 -0500

    404 en estructura

--- nuxt.config.js
+++ nuxt.config.js
@@ -8,13 +8,12 @@ export default {
       { hid: 'description', name: 'description', content: '' }
     ],
     link: [
-      { rel:'preconnect', href: 'https://fonts.gstatic.com'}
-    ],
-    link: [
-      { href: 'https://fonts.googleapis.com/css2?family=Goldman&display=swap', rel:'stylesheet' }
+      // {rel="preconnect", href="https://fonts.gstatic.com"},
+      { rel:'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Goldman&display=swap'},
+      {rel: 'icon', type: 'image/x-icon', href: '/favicon.ico'}
     ],    
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+      {  }
     ]
   },
 
--- pages/clientes.vue
+++ pages/clientes.vue
@@ -1,7 +1,12 @@
 <template>
   <div id="clientes">
     <encabezado />
-    <h3>Estos son nuestros clientes</h3>
+    <h3 class="fuente">
+      Estos son nuestrros clientes Lorem ipsum dolor sit amet consectetur
+      adipisicing elit. Aspernatur provident quidem, nemo earum ea voluptate
+      placeat, cupiditate eveniet tempore beatae voluptates numquam consequuntur
+      corporis esse facere magni facilis at nobis.
+    </h3>
     <customers />
     <pie />
   </div>
@@ -13,6 +18,9 @@ import pie from "../components/pie";
 import customers from "../components/customers";
 export default {
   name: "clientes",
+  head: {
+    title: "Nuestros Clientes"
+  },
   components: {
     encabezado,
     pie,
